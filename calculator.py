8b55bdb0ee16b23ca5b9a909106a2c05b0f09cce
Gavin DeYoung <74500753+zelot7763@users.noreply.github.com>
2025-11-17 19:47:16 -0500

    modified calculator.py, added math methods

--- calculator.py
+++ calculator.py
@@ -4,6 +4,24 @@ calculator.py
 
 One function per operation, in order.
 """
-# First example
+import math
+
 def add(a, b):
-    pass
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0: raise ZeroDivisionError
+    return b / a
+
+def log(a, b):
+    if b < 1: raise ValueError
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
