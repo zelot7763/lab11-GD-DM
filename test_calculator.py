73890d99fe4d4e469226fe86712a2bec5a12c2f8
Gavin DeYoung <74500753+zelot7763@users.noreply.github.com>
2025-11-18 21:47:46 -0500

    added partner 1 test methods

--- test_calculator.py
+++ test_calculator.py
@@ -11,11 +11,15 @@ class TestCalculator(unittest.TestCase):
     # ##########################
 
     ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
+    def test_multiply(self): # 3 assertions
+        self.assertEqual(mul(9, 9), 81)
+        self.assertEqual(mul(9123, 69348), 632661804)
+        self.assertEqual(mul(23.9885, 2.8967), 69.48748795)
 
-    # def test_divide(self): # 3 assertions
-    #     fill in code
+    def test_divide(self): # 3 assertions
+        self.assertEqual(div(5, 10), 2)
+        self.assertEqual(div(7, 95263), 13609)
+        self.assertEqual(div(89.4937, 23.889), 0.2669349909546705522)
     # ##########################
 
     ######## Partner 2
@@ -34,21 +38,22 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self): # 1 assertion
+        with self.assertRaises(ValueError):
+            log(0, 5)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self): # 3 assertions
+        self.assertEqual(hypotenuse(3, 4), 5)
+        self.assertEqual(hypotenuse(15, 20), 25)
+        self.assertAlmostEqual(hypotenuse(17, 53), 55.65968020030299)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
+    def test_sqrt(self): # 3 assertions
+        # Test for invalid argument, example:
+        with self.assertRaises(ValueError):
+            square_root(-1)
+        # Test basic function
+        self.assertEqual(square_root(144), 12)
+        self.assertEqual(square_root(952), 30.854497241083024978)
     ##########################
 
 # Do not touch this
